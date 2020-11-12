0669b1ddb662fac84d7f6a6176f4dccd6bfa43e3
Yahya Dereli <yahyadereli@gmail.com>
2020-11-13 00:43:11 +0300

    expressjs-api-first-commit

--- server.js
+++ server.js
@@ -4,12 +4,42 @@ const devMiddleware = require("webpack-dev-middleware");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { VueLoaderPlugin } = require("vue-loader");
 
+const fs = require("fs");
+// fs.readFile("./src/users.json", "utf8", function(err, data){
+//   if(err){
+//     console.log("hata")
+//   }
+//   const users = data;
+//   console.log(users)
+// })
 
+fs.readFile("./src/users.json", "utf8", function(err, data){
+  if(err){
+    console.log("hata")
+  }
+  const users = data;
+  const user = JSON.parse(users)
+  console.log(user.yahya)
+})
 
 const HOST = "0.0.0.0";
 const PORT = 8080;
 
 const app = express();
+
+app.get('/api', (req, res) => {
+  fs.readFile("./src/users.json", "utf8", function(err, data){
+    if(err){
+      console.log("hata")
+    }
+    const users = data;
+    // const user = JSON.parse(users)
+    // res.json(user)
+    res.json(users)
+  })
+})
+
+
 app.use(
   devMiddleware(
     webpack({
@@ -47,6 +77,9 @@ app.use(
     }
   )
 );
+
+
+
 app.listen(PORT, HOST, () => {
   console.log(`Node version is ${process.version}`);
   console.log(`Server running at http://${HOST}:${PORT}/`);
